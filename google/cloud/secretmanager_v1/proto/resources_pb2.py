07093f4b7e3219ca234a5118a206cf13ed30ea3b
yoshi-automation <yoshi-automation@google.com>
2020-05-30 08:09:05 -0700

    fix: inconsistent json and yaml spanner configs
    This tries to resolve inconsistent timeout/retries in different client
    libraries.
    
    Two major changes:
    - Update *_grpc_service_config.json and *_gapic.yaml to make sure that they
      are consistent.
    - Add missing ListDatabases config in spanner_admin_database_gapic.yaml.
    
    PiperOrigin-RevId: 312582296
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed May 20 16:55:15 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: d8a17933f650cf605196ef14f03edc246f0562b6
    Source-Link: https://github.com/googleapis/googleapis/commit/d8a17933f650cf605196ef14f03edc246f0562b6

--- google/cloud/secretmanager_v1/proto/resources_pb2.py
+++ google/cloud/secretmanager_v1/proto/resources_pb2.py
@@ -710,8 +710,7 @@ Replication = _reflection.GeneratedProtocolMessageType(
         ),
         "DESCRIPTOR": _REPLICATION,
         "__module__": "google.cloud.secretmanager_v1.proto.resources_pb2",
-        "__doc__": """A policy that defines the replication configuration of
-  data.
+        "__doc__": """A policy that defines the replication configuration of data.
   
   
   Attributes:
