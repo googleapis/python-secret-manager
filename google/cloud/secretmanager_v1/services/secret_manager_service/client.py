b3e04d6ec0a220f5e8662acb9bdfcb8edf6ec82c
yoshi-automation <yoshi-automation@google.com>
2021-03-10 03:29:19 -0800

    chore: upgrade gapic-generator-python to 0.42.2
    PiperOrigin-RevId: 361662015
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Mar 8 14:47:18 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 28a591963253d52ce3a25a918cafbdd9928de8cf
    Source-Link: https://github.com/googleapis/googleapis/commit/28a591963253d52ce3a25a918cafbdd9928de8cf

--- google/cloud/secretmanager_v1/services/secret_manager_service/client.py
+++ google/cloud/secretmanager_v1/services/secret_manager_service/client.py
@@ -1495,10 +1495,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.SetIamPolicyRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.SetIamPolicyRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
@@ -1600,10 +1603,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.GetIamPolicyRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.GetIamPolicyRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
@@ -1655,10 +1661,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.TestIamPermissionsRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.TestIamPermissionsRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
--- google/cloud/secretmanager_v1beta1/services/secret_manager_service/client.py
+++ google/cloud/secretmanager_v1beta1/services/secret_manager_service/client.py
@@ -1479,10 +1479,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.SetIamPolicyRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.SetIamPolicyRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
@@ -1584,10 +1587,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.GetIamPolicyRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.GetIamPolicyRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
@@ -1639,10 +1645,13 @@ class SecretManagerServiceClient(metaclass=SecretManagerServiceClientMeta):
         """
         # Create or coerce a protobuf request object.
 
-        # The request isn't a proto-plus wrapped type,
-        # so it must be constructed via keyword expansion.
         if isinstance(request, dict):
+            # The request isn't a proto-plus wrapped type,
+            # so it must be constructed via keyword expansion.
             request = iam_policy.TestIamPermissionsRequest(**request)
+        elif not request:
+            # Null request, just make one.
+            request = iam_policy.TestIamPermissionsRequest()
 
         # Wrap the RPC method; this adds retry and timeout information,
         # and friendly error handling.
