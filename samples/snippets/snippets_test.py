91d61c84879b3beb077c51a13b9d574375473c15
nataliesalvati <105504539+nataliesalvati@users.noreply.github.com>
2022-08-08 14:25:46 -0400

    Update snippets_test.py

--- samples/snippets/snippets_test.py
+++ samples/snippets/snippets_test.py
@@ -288,11 +288,11 @@ def test_consume_event_notification(pubsub_message):
 
 def test_update_secret_with_etag(secret):
     project_id, secret_id, etag = secret
-    secret = update_secret_with_etag(project_id, secret_id, etag)
-    assert secret.version_aliases["test"] == "1"
-
-def test_update_secret_with_alias(secret):
-    project_id, secret_id, version_id, _ = secret_version
-    project_id, secret_id, _ = secret
     secret = update_secret_with_etag(project_id, secret_id)
     assert secret.labels["secretmanager"] == "rocks"
+
+def test_update_secret_with_alias(secret, secret_version):
+    project_id, secret_id, version_id, _ = secret_version
+    project_id, secret_id, _ = secret
+    secret = update_secret_with_alias(project_id, secret_id)
+    assert secret.version_aliases["test"] == "1"
